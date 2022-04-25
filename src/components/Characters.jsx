230201fd916ad2ba4fee0cdbb95cf6531e480e63
John McFarland <Jmcfar187@west-mec.local>
2022-04-25 15:13:51 -0700

    styles

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -2,8 +2,8 @@ import axios from 'axios'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-class Characters extends React.Component{
-    constructor(props){
+class Characters extends React.Component {
+    constructor(props) {
         super(props)
 
         this.state = {
@@ -26,24 +26,38 @@ class Characters extends React.Component{
             console.log(error);
         }
 
-        this.setState({characters: result.data.results})
+        this.setState({ characters: result.data.results })
     }
 
 
 
-    render(){
+    render() {
 
-        const {characters} = this.state
+        const { characters } = this.state
 
         let mapArray = characters.map(character => {
 
             return <div className='character'>
-                <h1>{character.name}</h1>
+                <div className='pfp'>
+
+                    <img src={character.image} />
+                </div>
+                <div className='content'>
+
+                    <h2>{character.name}</h2>
+                    <div className='status'>
+                      <div className= {`status-icon ${(character.status == 'Alive' ? "green":"red")} ${(character.status == "unknown" ? "unknown":"")}`}></div>
+                      <p>{character.status}</p>
+                    </div>
+                    <p>Species: {character.species}</p>
+                    <p>Last Known Location: {character.location.name}</p>
+                </div>
+
             </div>
         })
 
-        return <div className='characters-container'>
-            
+        return <div className='character-container'>
+
             {mapArray}
         </div>
     }
