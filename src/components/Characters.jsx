3841e2084220e790f1d61dc1ab2cd36f8809cd85
jmac222 <john.mcfarland222@gmail.com>
2022-04-27 11:54:01 -0700

    fixed navbar

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,65 +1,66 @@
-import axios from 'axios'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from "axios";
+import React from "react";
+import { Link } from "react-router-dom";
+
 
 class Characters extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            characters: []
-        }
+    this.state = {
+      characters: [],
+    };
+  }
+
+  async componentDidMount() {
+    let url = "https://rickandmortyapi.com/api/character";
+    let result = null;
+
+    try {
+      result = await axios(url, {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+    } catch (error) {
+      console.log(error);
     }
 
+    this.setState({ characters: result.data.results });
+  }
 
-    async componentDidMount() {
-        let url = 'https://rickandmortyapi.com/api/character'
-        let result = null
+  render() {
+    const { characters } = this.state;
 
-        try {
-            result = await axios(url, {
-                headers: {
-                    Accept: 'application/json'
-                }
-            })
-        } catch (error) {
-            console.log(error);
-        }
-
-        this.setState({ characters: result.data.results })
-    }
-
-
-
-    render() {
-
-        const { characters } = this.state
-
-        let mapArray = characters.map(character => {
-
-            return <div className='character'>
-                <div className='pfp'>
-
-                    <img src={character.image} />
-                </div>
-                <div className='content'>
-
-                    <h2>{character.name}</h2>
-                    <div className='status'>
-                      <div className= {`status-icon ${(character.status == 'Alive' ? "green":"red")} ${(character.status == "unknown" ? "unknown":"")}`}></div>
-                      <p>{character.status}</p>
-                    </div>
-                    <p>Species: {character.species}</p>
-                    <p>Last Known Location: {character.location.name}</p>
-                </div>
-
+    let mapArray = characters.map((character) => {
+      return (
+        <div className="character">
+          <div className="pfp">
+            <img src={character.image} />
+          </div>
+          <div className="content">
+            <h2>{character.name}</h2>
+            <div className="status">
+              <div
+                className={`status-icon ${
+                  character.status == "Alive" ? "green" : "red"
+                } ${character.status == "unknown" ? "unknown" : ""}`}
+              ></div>
+              <p>{character.status}</p>
             </div>
-        })
-
-        return <div className='character-container'>
-
-            {mapArray}
+            <p>Species: {character.species}</p>
+            <p>Last Known Location: {character.location.name}</p>
+          </div>
         </div>
-    }
+      );
+    });
+
+    return (
+      <>
+      
+        <div className="character-container">{mapArray}</div>
+      </>
+    );
+  }
 }
-export default Characters
+export default Characters;
